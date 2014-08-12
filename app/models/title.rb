59b906fa0626a6659f2ceab1844d56ab702ebbf0
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2014-08-11 18:36:59 -0700

    Small fixes

--- app/models/title.rb
+++ app/models/title.rb
@@ -5,14 +5,14 @@ class Title < ActiveRecord::Base
     foreign_key: :movie_id
   has_many :title_keywords, foreign_key: :movie_id
   has_many :keywords, through: :title_keywords
-  has_many :infos, class_name: 'TitleInfo'
+  has_many :infos, class_name: 'TitleInfo', foreign_key: :movie_id
 
   # For each of the roles in CastInfo, we define a scoped method here
   # so we have title.directors, title.actors, etc.
   CastInfo.roles.keys.each do |role|
-    define_method(role.pluralize) {
+    define_method(role.pluralize) do
       persons.merge(CastInfo.send(role))
-    }
+    end
   end
 
   # as above, but not gender-specific
