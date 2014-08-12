ef15b80e87ae5f12a52eb48098ac4d0ada1bd7d3
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2014-08-11 21:54:09 -0700

    Add methods useful for views

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,9 @@ class Person < ActiveRecord::Base
   has_many :cast_infos
   has_many :titles, through: :cast_infos
   has_many :infos, class_name: 'PersonInfo'
+
+  def natural_name
+    last, rest = name.split(', ')
+    [rest, last].join(' ')
+  end
 end
--- app/models/title_info.rb
+++ app/models/title_info.rb
@@ -1,4 +1,9 @@
 class TitleInfo < ActiveRecord::Base
   belongs_to :title, foreign_key: :movie_id
   belongs_to :info_type
+
+  InfoType.pluck(:id, :info).each do |info_type_id, info_type_name|
+    scope info_type_name.tr(' ', '_'),
+      -> { where(info_type_id: info_type_id) }
+  end
 end
