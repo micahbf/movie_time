a4484d2851f6222c14c94ec57d2475180d3d2606
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2014-08-11 22:03:40 -0700

    Comments are nice

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,6 +3,7 @@ class Person < ActiveRecord::Base
   has_many :titles, through: :cast_infos
   has_many :infos, class_name: 'PersonInfo'
 
+  # Connery, Sean becomes Sean Connery
   def natural_name
     last, rest = name.split(', ')
     [rest, last].join(' ')
--- app/models/title_info.rb
+++ app/models/title_info.rb
@@ -2,6 +2,7 @@ class TitleInfo < ActiveRecord::Base
   belongs_to :title, foreign_key: :movie_id
   belongs_to :info_type
 
+  # Scopes for every InfoType
   InfoType.pluck(:id, :info).each do |info_type_id, info_type_name|
     scope info_type_name.tr(' ', '_'),
       -> { where(info_type_id: info_type_id) }
