6be5945b848acd863ae8c6519af03c4581038ec8
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2014-08-11 22:49:09 -0700

    Add index action and view

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -2,4 +2,10 @@ class TitlesController < ApplicationController
   def show
     @title = Title.find(params[:id])
   end
+
+  def index
+    @titles = Title.order(:title)
+    @titles = @titles.genre(params[:genre]) if params[:genre]
+    @titles = @titles.page(params[:page])
+  end
 end
